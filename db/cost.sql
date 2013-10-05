b038100ecf696578e13516f6ef8953a8d4847c01
Suchit Parikh <suchit.parikh@sendgrid.com>
2013-10-05 09:42:41 -0700

    Some reorg

--- db/cost.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `cost`;
-
-CREATE TABLE `cost` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
-  `card_part` varchar(255) DEFAULT NULL,
-  `cost` decimal(5,2) NOT NULL DEFAULT 0.0,
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
